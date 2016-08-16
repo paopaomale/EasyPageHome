d6a7db470ce4a51905603c484247230f104ce4c1
paopao <paopapmale@gmail.com>
2016-08-16 16:34:22 +0800

    init

--- EasyInvest-H5/butterfly/js/components/listview/ListViewTemplateItem.js
+++ EasyInvest-H5/butterfly/js/components/listview/ListViewTemplateItem.js
@@ -1,13 +1,1 @@
-define(['./ListViewItem'], function(ListViewItem){
-	
-	return ListViewItem.extend({
-
-		initialize: function(options){
-			ListViewItem.prototype.initialize.call(this, options);
-			//render the template into HTML, append to self
-			// this.el.innerHTML = this.template(options.data);
-
-			this.setContent(this.template(options.data));
-		}
-	});
-});
+define(["./ListViewItem"],function(a){return a.extend({initialize:function(b){a.prototype.initialize.call(this,b),this.setContent(this.template(b.data))}})});
